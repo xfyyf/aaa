cc9c9fc380063724acc2efea6b1592eb3f459db1
xyyf <xfyyf@163.com>
2017-11-10 17:06:11 +0800

    实现Emp.java

--- demoapp/src/main/java/demo/bean/Emp.java
+++ demoapp/src/main/java/demo/bean/Emp.java
@@ -1,5 +1,20 @@
 package demo.bean;
 
 public class Emp {
-
+   private Integer eid;
+   private String ename;
+public Integer getEid() {
+	return eid;
+}
+public void setEid(Integer eid) {
+	this.eid = eid;
+}
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+   
+   
 }
